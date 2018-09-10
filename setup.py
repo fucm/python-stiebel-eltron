5a7ebba8d4f79d77bd709324251dd73b483a9455
fucm <fucm@users.noreply.github.com>
2018-09-10 22:43:48 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Copyright (c) 2018 Martin Fuchs <mail-addr>
+Licensed under MIT. All rights reserved.
+"""
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='python-stiebel-eltron',
+    version='0.0.1.dev0',
+    description='Python API for interacting with the Stiebel Eltron ISG web gateway via Modbus for controlling integral ventilation units and heat pumps.',
+    long_description=long_description,
+    url='https://github.com/fucm/python-stiebel-eltron',
+    author='Martin Fuchs',
+    author_email='mail-addr',
+    license='MIT',
+    install_requires=[],
+    packages=find_packages(),
+    zip_safe=True,
+    include_package_data=True,
+    """ https://pypi.org/classifiers/ """
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+)
