611914f2cbbed312ebcd0a279aa657d45921c6bf
fucm <fucm@users.noreply.github.com>
2019-02-08 23:00:18 +0100

    Fix pypi description

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+if sys.argv[-1] == 'build':
+    os.system('python3 setup.py sdist bdist_wheel')
+    sys.exit()
+
 if sys.argv[-1] == 'publish':
     os.system('python3 setup.py sdist upload')
     sys.exit()
@@ -32,14 +36,15 @@ class Tox(TestCommand):
 
 
 setup(
-    name='pyStiebelEltron',
+    name='pystiebeleltron',
     version='0.0.1.dev0',
     description='Python API for interacting with the Stiebel Eltron ISG web gateway via Modbus for controlling integral ventilation units and heat pumps.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/fucm/python-stiebel-eltron',
     author='Martin Fuchs',
-    author_email='martin.fuchs AT gmx.ch',
     license='MIT',
+    python_requires='>=3.4',
     install_requires=['pymodbus>=2.1.0'],
     tests_require=['tox'],
     cmdclass={'test': Tox},
